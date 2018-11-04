42cefc59e753f67a9f046a89502b9c9df165a7e1
Yanqi Xu <yanqixu@outlook.com>
2018-11-04 00:02:57 -0500

    scraping the page of MHP

---
+++ highwaypatrol/highwaypatrol.py
@@ -0,0 +1,38 @@
+#highwapatrol notes Notes
+#import the library used for scraping
+import requests, mechanize
+#import the BeautifulSoup library
+from bs4 import BeautifulSoup
+
+#assign the string of the website to a variable called "url"
+url = 'https://www.mshp.dps.missouri.gov/HP71/search.jsp'
+
+# Get the HTML of the page
+#define the br function for browser
+br = mechanize.Browser()
+#use the br and add a dot notation 
+br.open(url)
+# used the functinos to define the html
+html = br.response().read()
+
+# Transform the HTML into a BeautifulSoup object and parse it
+soup = BeautifulSoup(html, "html.parser")
+
+# Find what the tables contain
+# The function identifies the objects for scraping by their ids
+main_table = soup.find('table',
+    {'class': 'accidentOutput'}
+)
+# Use the findall() methodto loop through every <tr>element 
+row_list = main_table.find_all('tr')
+
+# Use a for loop to get the data from each table row
+for r in row_list:
+# Find the individual entries in the cells of the same row
+    cell_list = r.find_all('td',{'class': 'infoCell'})
+# If the row is not empty, then find what's in the cells and print them one by one, also removing whitespaces
+    if len(cell_list) > 0:
+        for c in cell_list:
+            print c.text.strip()
+# Add a divider in the form of '----------'
+        print '----------'
