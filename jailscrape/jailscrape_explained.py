155adea5bde6a0d578dc9e4df00b2a52c51e3001
Yanqi Xu <yanqixu@outlook.com>
2019-12-29 14:23:03 -0500

    fix iterator and update csv

--- jailscrape/jailscrape_explained.py
+++ jailscrape/jailscrape_explained.py
@@ -1,15 +1,16 @@
-#import the library used for scraping
-import requests, mechanize
-#import the BeautifulSoup library
+# import the library used for scraping
+import requests
+import mechanize
+# import the BeautifulSoup library
 from bs4 import BeautifulSoup
 
-#assign the string of the website to a variable called "url"
-url = 'https://report.boonecountymo.org/mrcjava/servlet/SH01_MP.I00290s?max_rows=500'
+# assign the string of the website to a variable called "url"
+url = 'https://www.njconsumeraffairs.gov/phar/Pages/actions.aspx'
 
 # Get the HTML of the page
-#define the br function for browser
+# define the br function for browser
 br = mechanize.Browser()
-#use the br and add a dot notation 
+# use the br and add a dot notation
 br.open(url)
 # used the functinos to define the html
 html = br.response().read()
@@ -20,20 +21,20 @@ soup = BeautifulSoup(html, "html.parser")
 # Find what the tables contain
 # The function identifies the objects for scraping by their ids
 main_table = soup.find('tbody',
-    {'id': 'mrc_main_table'}
-)
-# Use the findall() methodto loop through every <tr>element 
-row_list = main_table.find_all('tr')
-
+                       #{'class': 'mrc_main_table'}
+                       )
+# Use the findall() methodto loop through every <tr>element
+#row_list = main_table.find_all('tr')
+print(main_table)
 # Use a for loop to get the data from each table row
-for r in row_list:
+# for r in row_list:
 # Find the individual entries in the cells of the same row
-    cell_list = r.find_all('td')
+#cell_list = r.find_all('td')
 # If the row is not empty, then find what's in the cells and print them one by one, also removing whitespaces
-    if len(cell_list) > 0:
+"""     if len(cell_list) > 0:
         for c in cell_list:
-            print c.text.strip()
+            print c.text.strip() """
 # Add a divider in the form of '----------'
-        print '----------'
+# print '----------'
 
-print 'IT WORKED!'
+print('IT WORKED!')
